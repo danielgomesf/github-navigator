b823d70a0a22068a61900eb19752d6f08430722f
Daniel Gomes Falcao <daniel.falcao@RJ-MB5264.local>
2023-01-14 16:56:25 -0300

    Created page with repo details

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,48 +1,78 @@
-import React, { useState, useCallback } from "react";
-import { FaGithub, FaPlus, FaSpinner } from 'react-icons/fa';
-import { Container, Form, SubmitButton } from './styles';
-
+import React, { useState, useCallback, useEffect } from "react";
+import { FaGithub, FaPlus, FaSpinner, FaBars, FaTrash } from 'react-icons/fa';
+import { Container, Form, SubmitButton, List, DeleteButton } from './styles';
+import { Link } from "react-router-dom";
 import api from "../../services/api";
 
 export default function Main() {
 
     const [ newRepo, setNewRepo ] = useState('');
-    const [ repo, setRepo ] = useState([]);
+    const [ repositories, setRepositories] = useState([]);
     const [ loading, setLoading ] = useState(false);
+    const [ alert, setAlert ] = useState(null);
+
+    useEffect(()=>{
+        const repoStorage = localStorage.getItem('repos');
+    
+        if(repoStorage){
+          setRepositories(JSON.parse(repoStorage));
+        }
+    
+      }, []);
+    
+      useEffect(()=>{
+        localStorage.setItem('repos', JSON.stringify(repositories));
+      }, [repositories]);
 
     const handleSubmit = useCallback((e) => {
         e.preventDefault();
 
-        async function submit() {
-
+        async function submit(){
+            setLoading(true);
+            setAlert(null);
             try{
-                setLoading(true);
-                const response = await api.get(`repos/${newRepo}`)
+
+                if(newRepo === ''){
+                    throw new Error('You need to enter a repository!')
+                }
+
+                const hasRepo = repositories.find(repo => repo.name === newRepo);
+
+                if(hasRepo) {
+                    throw new Error('Repository has already been added')
+                }
+
+                const response = await api.get(`repos/${newRepo}`);
 
                 const data = {
                     name: response.data.full_name,
                 }
 
-                setRepo([...repo, data ]);
+                setRepositories([...repositories, data ]);
                 setNewRepo('');
-            }
-            catch(error) {
+            }catch(error){
+                setAlert(true);
                 console.log(error);
             }
             finally{
                 setLoading(false);
             }
 
-
         }
 
         submit();
 
-    }, [newRepo, repo]);
+    }, [newRepo, repositories]);
 
     function handleInputChange(e) {
         setNewRepo(e.target.value);
+        setAlert(null);
     }
+
+    const handleDelete = useCallback((repo)=> {
+        const find = repositories.filter(r => r.name !== repo);
+        setRepositories(find);
+    }, [repositories]);
 
     return(
         <Container>
@@ -51,7 +81,7 @@ export default function Main() {
                 My Repositories
             </h1>
 
-            <Form onSubmit={handleSubmit}>
+            <Form onSubmit={handleSubmit} error={alert}> 
                 <input 
                     type="text" 
                     placeholder="Add Repositories" 
@@ -67,6 +97,22 @@ export default function Main() {
                     )}
                 </SubmitButton>
             </Form>
+
+            <List>
+                {repositories.map(repo => (
+                    <li key={repo.name}>
+                        <span>
+                            <DeleteButton onClick={() => handleDelete(repo.name)} >
+                                <FaTrash size={14}/>
+                            </DeleteButton>
+                            {repo.name}
+                        </span>
+                        <Link to={`/repo/${encodeURIComponent(repo.name)}`}>
+                            <FaBars size={20} />
+                        </Link>
+                    </li>
+                ))}
+            </List>
 
         </Container>
     )
--- src/pages/Repo/index.js
+++ src/pages/Repo/index.js
@@ -1,7 +1,91 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Container, Loading ,Owner, BackButton, IssuesList } from "../Repo/styles";
+import { FaArrowLeft } from 'react-icons/fa'
+import api from "../../services/api";
 
-export default function Repo() {
+export default function Repo({match}) {
+
+    const [ repository, setRepository ] = useState({});
+    const [ issues, setIssues ] = useState([]);
+    const [ loading, setLoading ] = useState(true);
+
+    useEffect(() => {
+
+        async function load(){
+            const nameRepo = decodeURIComponent(match.params.repo);
+
+            const [ repoData, issuesData ] = await Promise.all([
+                api.get(`/repos/${nameRepo}`),
+                api.get(`/repos/${nameRepo}/issues`, {
+                    params: {
+                        state: 'open',
+                        per_page: 5
+                    }
+                })
+            ]);
+
+            setRepository(repoData.data);
+            setIssues(issuesData.data);
+            setLoading(false);
+
+
+        };
+
+        load()
+
+    }, [match.params.repo])
+
+
+    if(loading) {
+        return(
+            <Loading>
+                <h1>Loading...</h1>
+            </Loading>
+        )
+    }
+
     return(
-        <h1>Repo</h1>
+        <Container>
+            <BackButton to="/">
+                <FaArrowLeft color="#000" size={30} />
+            </BackButton>
+            <Owner>
+                <img 
+                    src={repository.owner.avatar_url} 
+                    alt={repository.owner.login} 
+                />
+                <h1>
+                    {repository.name}
+                </h1>
+                <p>
+                    {repository.description}
+                </p>
+            </Owner>
+
+            <IssuesList>
+                {issues.map(issue => (
+                    <li key={String(issue.id)}>
+                        <img 
+                            src={issue.user.avatar_url} 
+                            alt={issue.user.login} 
+                        />
+                        <div>
+                            <strong>
+                                <a href={issue.html_url}>{issue.title}</a>
+
+                                {issue.labels.map(label => (
+                                    <span key={String(label.id)}>
+                                        {label.name}
+                                    </span>
+                                ))}
+                            </strong>
+                            <p>
+                                {issue.user.login}
+                            </p>
+                        </div>
+                    </li>
+                ))}
+            </IssuesList>
+        </Container>
     )
 }
